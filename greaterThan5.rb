c730858435d7669ce3954aec331f0bf04bc466d8
Ricardo <ricardo.cabrera.rivera@gmail.com>
2023-08-30 11:22:14 -0700

    numbers greater than 5

---
+++ greaterThan5.rb
@@ -0,0 +1,16 @@
+numbers = [5,1,4,22,3,0,9,8,56,345,7,2,7,5,3,8]
+count = 0
+
+numbers.each do |number|
+    if(number > 5)
+        puts "The number #{number} is greater than five"
+        count+=1
+
+    elsif(number == 5)
+        puts "The number #{number} is equal to five"
+    else
+        puts "The number #{number} is lower than five"
+    end
+end
+
+puts "#{count} numbers are greater than five"
