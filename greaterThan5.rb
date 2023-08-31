8f8816ace85b04e4ee68f8b1476547991cca806e
Ricardo <ricardo.cabrera.rivera@gmail.com>
2023-08-30 22:01:48 -0700

    adding a new array to fiil up

--- greaterThan5.rb
+++ greaterThan5.rb
@@ -1,9 +1,11 @@
 numbers = [5,1,4,22,3,0,9,8,56,345,7,2,7,5,3,8]
 count = 0
+greaterThanFive = []
 
 numbers.each do |number|
     if(number > 5)
         puts "The number #{number} is greater than five"
+        greaterThanFive.append(number)
         count+=1
 
     elsif(number == 5)
@@ -14,3 +16,4 @@ numbers.each do |number|
 end
 
 puts "#{count} numbers are greater than five"
+puts "Numbers that are > 5: #{greaterThanFive.sort}"
